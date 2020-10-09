bc558ceb5a4395e035db14fc6bc49d014258d613
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-10-10 00:53:09 +0300

    Update MaximumSumSequence

---
+++ MaximumSumSequence/cmake-build-debug/CMakeFiles/MaximumSumSequence.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MaximumSumSequence.dir/main.c.o"
+  "MaximumSumSequence"
+  "MaximumSumSequence.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MaximumSumSequence.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
