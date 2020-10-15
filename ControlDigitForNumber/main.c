984138c58e35528d5faff30b7cb002499635b13f
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-10-15 19:53:18 +0300

    Update

--- ControlDigitForNumber/main.c
+++
@@ -1,9 +0,0 @@
-#include "Testing/TestingImplementation.h"
-
-int main() {
-
-    Test_GetSumOfDigitsForNumber_();
-    Test_GetControlDigitForNumber_();
-
-    return 0;
-}
--- MaximumSumSequence/main.c
+++
@@ -1,9 +0,0 @@
-#include "Testing/TestingImplementation.h"
-
-int main() {
-
-    Test_GetMaxOfTwoNumbers_();
-    Test_GetMaximumSumSequence_();
-
-    return 0;
-}
